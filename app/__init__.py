6760c7f1a2f6cb4866deb928d0ae3c338ac4387d
jepkess <biivincent1997@gmail.com>
2021-11-06 16:27:09 +0300

    import auth as the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,10 @@ db=SQLAlchemy()
 
 def create_app(config_name):
   app=Flask(__name__)
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+  
 
 
   #creating the app configurations
