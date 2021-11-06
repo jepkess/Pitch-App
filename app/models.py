791da880ba7394d8b2ecb8c9d77203c1eb411dae
jepkess <biivincent1997@gmail.com>
2021-11-06 16:02:03 +0300

    downgrade the werkzeug application and perfrom password hashing

--- app/models.py
+++ app/models.py
@@ -1,9 +1,23 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model): #de.model connect our class to the database and allow communication.
     __tablename__ = 'users'
     id=db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(255))
+    pass_secure=db.Column(db.String(255)) #column for password authentication.
+
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
     def __rept__(self):
         return f'User {self.username}'
