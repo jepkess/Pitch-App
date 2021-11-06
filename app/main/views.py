b79ede6a57de2e72e0776e3f5ad959ad27417c91
jepkess <biivincent1997@gmail.com>
2021-11-06 19:51:24 +0300

    import login_required

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,17 @@
 from flask import render_template
 from . import main
+from flask_login import login_required
 
 #views
-
+@login_required
 @main.route('/')
 def index():
     """
     function that return the index page and its data
     """
     return render_template('index.html')
+    
+
+
 
    
