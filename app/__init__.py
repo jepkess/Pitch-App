9bcedf91dd5e6004ecbbc60d16f9733eac915adf
jepkess <biivincent1997@gmail.com>
2021-11-06 19:47:52 +0300

    import login manager  and register

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,18 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy #SQLAlchemy is a mapper that connect the application to the database.
+from flask_login import LoginManager
 
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login' 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
 
+
 def create_app(config_name):
   app=Flask(__name__)
-  from .auth import auth as auth_blueprint
-  app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
-  
-
 
   #creating the app configurations
   app.config.from_object(config_options[config_name])
@@ -21,11 +21,19 @@ def create_app(config_name):
   #Initializing the flask-extensions
   bootstrap.init_app(app) #calling the init_app on the extension to complete the initialization.
   db.init_app(app)
+  login_manager.init_app(app)
 
-  #Registering the Blueprint
+  #Registering the main Blueprint
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
 
+
+  #Registering the auth Blueprint
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+  
+
+   
   return app
 
   
