aeadbded1e63aa77ede67a5b3fcff8c5452cb8a6
jepkess <biivincent1997@gmail.com>
2021-11-08 00:30:54 +0300

    migrate the bio and  profile_pic_path to the database

--- app/models.py
+++ app/models.py
@@ -15,6 +15,8 @@ class User(UserMixin,db.Model): #de.model connect our class to the database and 
     username=db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     pitches=db.relationship('Pitches',backref='user',lazy="dynamic")
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     pass_secure=db.Column(db.String(255)) #column for password authentication.
     comments=db.relationship('Comments',backref='user',lazy="dynamic")
    
@@ -59,25 +61,6 @@ class Pitches(db.Model):
         
         def __repr__(self):
          return f'Comments: {self.pitch_title}'
-
-# class Comments(db.Model):
-#         __tablename__='comments'
-#         id=db.Column(db.Integer, primary_key=True)
-#         comment=db.Column(db.Text())
-#         user_id=db.Column(db.Integer, db.ForeignKey('users_id')) 
-#         pitch_id=db.Column(db.Integer, db.ForeignKey('pitches_id'))
-
-#         def save_comment(self):
-#           db.session.add(self)
-#           db.session.commit() 
-
-#         @classmethod
-#         def get_comment(cls,pitch_id):
-#           comment = Comments.query.filter_by(pitch_id=pitch_id).all()
-#           return comment  
-
-#         def __repr__(self):
-#           return f'Comments: {self.comment}'
 
 class Comments(db.Model):
     __tablename__ = 'comments'
