c57dee9633c522c26d3ed44a783a64cf6bb757f9
jepkess <biivincent1997@gmail.com>
2021-11-07 21:42:40 +0300

    create a pitch form and comments

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField,TextAreaField,SubmitField
+from wtforms import validators
+from wtforms.validators import required
+
+class PitchForm(FlaskForm):
+    title=StringField('Pitch Title', [validators.DataRequired(message='Field required')])
+    category=SelectField('Category',choices=[('PRAYER','PRAYER'),('NEWS','NEWS'),('RELATIONSHIP','RELATIONSHIP')])
+    description=TextAreaField('Pitch Description', [validators.DataRequired(message='Field required')])
+    submit=SubmitField('submit')
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Comment',  [validators.DataRequired(message='Field required')])
+    submit = SubmitField('Submit')
