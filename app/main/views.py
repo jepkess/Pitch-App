e8a30e34fa0dd0d921ea9eec5600113773208a02
jepkess <biivincent1997@gmail.com>
2021-11-08 00:33:12 +0300

    import updateprofile on the views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
-from flask import render_template,redirect,url_for
+from flask import render_template,redirect,url_for,abort
 from . import main
 from flask_login import login_required,current_user
-from .forms import PitchForm,CommentsForm
+from .forms import PitchForm,CommentsForm,UpdateProfile
 from ..models import Pitches, Comments,User
 from .. import db
 
@@ -62,6 +62,16 @@ def single_pitch():
         
     
     return render_template('pitch.html', pitches=all_pitches,commentform=form,comments=comments)
+
+#user profile function
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)        
      
     
 
