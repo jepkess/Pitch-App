cfdcd6617a138d004534897f725002dbf5effbcb
jepkess <biivincent1997@gmail.com>
2021-11-08 11:25:56 +0300

    import uploadset,configure-uploads and images from the flask-uploads

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_sqlalchemy import SQLAlchemy #SQLAlchemy is a mapper that connect the application to the database.
 from flask_login import LoginManager
+import os
+from flask_mail import Mail
+
 
 
 login_manager = LoginManager()
@@ -10,6 +14,9 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login' 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
+photos=UploadSet('photos',IMAGES)
+mail=Mail()
+
 
 
 def create_app(config_name):
@@ -18,10 +25,15 @@ def create_app(config_name):
   #creating the app configurations
   app.config.from_object(config_options[config_name])
 
+  #configure the uploadset
+  # configure_uploads(app,photos)
+  app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()
+
   #Initializing the flask-extensions
   bootstrap.init_app(app) #calling the init_app on the extension to complete the initialization.
   db.init_app(app)
   login_manager.init_app(app)
+  mail.init_app(app)
 
   #Registering the main Blueprint
   from .main import main as main_blueprint
