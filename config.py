0e603f66c1e92f23a34938614052c1fa8c698af7
jepkess <biivincent1997@gmail.com>
2021-11-08 16:55:44 +0300

    deploy

--- config.py
+++ config.py
@@ -16,6 +16,8 @@ class config:
 
 class Prodconfig(config):
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postges://"):
+        SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace("postgres://","postgesql://", 1)
     """
     configuring the application on the production mode.
     """ 
