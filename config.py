a4e03351f5999b624e31508e77c8bc2cea231ef4
jepkess <biivincent1997@gmail.com>
2021-11-08 11:23:57 +0300

    configuring email in the config.py

--- config.py
+++ config.py
@@ -4,6 +4,14 @@ class config:
     SECRET_KEY =os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:1234@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
+    UPLOADED_PHOTOS_SET='app/static/photos'
+
+    #Email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class Prodconfig(config):
