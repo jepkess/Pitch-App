0449c9a13b6987a4cbb8bd8f5626f3bd25526bd6
jepkess <biivincent1997@gmail.com>
2021-11-08 17:51:24 +0300

    fix the bugs

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ class config:
 
 class Prodconfig(config):
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postges://"):
-        SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace("postgres://","postgesql://", 1)
+    # if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postges://"):
+    #     SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace("postgres://","postgesql://", 1)
     """
     configuring the application on the production mode.
     """ 
-    
+    pass
 
 class Devconfig(config):
     DEBUG=True
